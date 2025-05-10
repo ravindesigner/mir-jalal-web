2d968a156dceb3db3886fa9aedfdf5d274677fc7
ravindesigner <67923121+ravindesigner@users.noreply.github.com>
2025-05-10 22:44:47 +0300

    Add files via upload

---
+++ demo/myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render,HttpResponse
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
